2eca29a33f3f2274ea1f7ececffe00e4fae32daf
Areeba Jamil <101094303+Areeba-Jamil@users.noreply.github.com>
2022-06-06 15:15:16 +0500

    Word Dictionary

--- Task12/WordDictionary.py
+++ Task12/WordDictionary.py
@@ -9,31 +9,32 @@ dictionary = PyDictionary()
 win =Tk()
 
 #Define the size of the window
-win.geometry("700x400")
+win.geometry("650x300")
 
-win.title("Python Dictionary")
+win.title("Python Word Dictionary")
 
 #Define Helper Function to use the other atributes of PyDictionary Class
 def dict():
    meaning.config(text=dictionary.meaning(word.get())['Noun'][0])
 
 #Define Labels and Buttons
-Label(win, text="Dictionary", font=("Times New Roman" ,20)).pack(pady=20)
+Label(win, text="Word Dictionary", font=("Times New Roman" ,24)).pack(pady=20)
 
 # Frame 1
 frame = Frame(win)
-Label(frame, text="Type any Word ", font=("Poppins bold", 15)).pack(side=LEFT)
-word = Entry(frame, font=("Times New Roman", 15))
+Label(frame, text="Enter a Word ", font=("Times New Roman", 14)).pack(side=LEFT)
+word = Entry(frame, font=("Times New Roman", 12))
 word.pack()
 frame.pack(pady=10)
+
 # Frame 2
 frame1 = Frame(win)
-Label(frame1, text="Meaning:", font=("Aerial", 18)).pack(side=LEFT)
-meaning = Label(frame1, text="", font=("Poppins",15), width= 30)
+Label(frame1, text="Meaning:", font=("Times New Roman", 14)).pack(side=LEFT)
+meaning = Label(frame1, text="", font=("Times New Roman",12,"italic"), width= 50)
 meaning.pack()
 frame1.pack(pady=10)
 
-Button(win, text="Find", font=("Poppins bold",15), command=dict).pack()
+Button(win, text="Search Meaning", font=("Times New Roman",15), command=dict).pack()
 
 # Execute Tkinter
 win.mainloop()
